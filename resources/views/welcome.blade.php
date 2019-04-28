5e9fd5cd062b23716b1f583b1918d31591e75459
AlexHff <hello.alex.buzz@gmail.com>
2019-04-28 19:04:14 +0200

    Add PageController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ECE
                 </div>
 
                 <div class="links">
